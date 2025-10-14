ed02dcabd3eacc289b0a2585d30bb3f1f71225ab
Ilham Putra Arysila <ilhamputraaris22@gmail.com>
2025-10-14 09:38:40 +0700

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:intl/intl.dart'; // Pastikan package intl: ^0.18.1 sudah ditambahkan di pubspec.yaml
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const AnnibaGiftApp());
 }
 
